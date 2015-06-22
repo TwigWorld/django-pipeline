5c26ec913ed0735ef209419e303eed70997ad500
Linus Heckemann <lheckemann@twig-world.com>
2015-06-22 17:15:51 +0100

    Add check for outdated in less compiler

--- pipeline/compilers/less.py
+++ pipeline/compilers/less.py
@@ -13,7 +13,10 @@ class LessCompiler(SubProcessCompiler):
         return filename.endswith('.less')
 
     def compile_file(self, infile, outfile, outdated=False, force=False):
-        command = "%s %s %s %s" % (
+        if not (outdated or force):
+            return
+        # Pipe to file rather than provide outfile arg due to a bug in lessc
+        command = "%s %s %s > %s" % (
             settings.PIPELINE_LESS_BINARY,
             settings.PIPELINE_LESS_ARGUMENTS,
             infile,
