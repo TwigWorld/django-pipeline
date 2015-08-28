c1b8768ee7a8826f3edd530dfd7054828c720020
Mark Provan <markgprovan@gmail.com>
2015-08-28 12:53:09 +0100

    Fixes context bug when 500 error occurs

--- pipeline/templatetags/compressed.py
+++ pipeline/templatetags/compressed.py
@@ -53,11 +53,15 @@ class CompressedCSSNode(CompressedMixin, template.Node):
 
     def render(self, context):
         package_name = template.Variable(self.name).resolve(context)
+
         try:
             package = self.package_for(package_name, 'css')
-            self.gzip = self.gzip_allowed(context['request'].META.get('HTTP_ACCEPT_ENCODING', ''))
         except PackageNotFound:
             return ''  # fail silently, do not return anything if an invalid group is specified
+
+        if context.get('request', False):
+            self.gzip = self.gzip_allowed(context['request'].META.get('HTTP_ACCEPT_ENCODING', ''))
+
         return self.render_compressed(package, 'css')
 
     def render_css(self, package, path):
@@ -85,11 +89,15 @@ class CompressedJSNode(CompressedMixin, template.Node):
 
     def render(self, context):
         package_name = template.Variable(self.name).resolve(context)
+
         try:
             package = self.package_for(package_name, 'js')
-            self.gzip = self.gzip_allowed(context['request'].META.get('HTTP_ACCEPT_ENCODING', ''))
         except PackageNotFound:
             return ''  # fail silently, do not return anything if an invalid group is specified
+        
+        if context.get('request', False):
+            self.gzip = self.gzip_allowed(context['request'].META.get('HTTP_ACCEPT_ENCODING', ''))
+
         return self.render_compressed(package, 'js')
 
     def render_js(self, package, path):
